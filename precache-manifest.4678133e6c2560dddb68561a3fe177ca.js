56a8a3bddbea31125e4109a8793e32a7f5c09bd3
unknown <christophe.navarro@epitech.eu>
2020-07-07 14:58:19 +0200

    Updates

---
+++ precache-manifest.4678133e6c2560dddb68561a3fe177ca.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6ff957792ee2ce0a225757438b06215",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "0db8334e1d207d5f63bb",
+    "url": "/Portfolio/static/css/2.4b36e02c.chunk.css"
+  },
+  {
+    "revision": "0e538edea431dff9a3d3",
+    "url": "/Portfolio/static/css/main.d52d1657.chunk.css"
+  },
+  {
+    "revision": "0db8334e1d207d5f63bb",
+    "url": "/Portfolio/static/js/2.f197810c.chunk.js"
+  },
+  {
+    "revision": "339bc2301027dc6f62f3170009b3a391",
+    "url": "/Portfolio/static/js/2.f197810c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e538edea431dff9a3d3",
+    "url": "/Portfolio/static/js/main.a4a2ac33.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  },
+  {
+    "revision": "7175ab51301266b91a530213bf6063ad",
+    "url": "/Portfolio/static/media/eau.7175ab51.jpg"
+  },
+  {
+    "revision": "c9b67ee7a003b03d83c72cf8983ea510",
+    "url": "/Portfolio/static/media/texture-background.c9b67ee7.jpg"
+  }
+]);
